52920cd10a82ce95c9a14fc44752646b45f21ed0
Shashankgg11 <shashankgg11@gmail.com>
2026-08-18 14:31:42 +0530

    added mbits vip

--- tb/cpu_agent/cpu_write_seq.sv
+++ tb/cpu_agent/cpu_write_seq.sv
@@ -19,14 +19,14 @@ class cpu_write_seq extends uvm_sequence #(cpu_tx);
       pkt_type == WRITE_PKT;
       wr_en == 1;
       rd_en == 0;
-      size == 2;
+      size == 0;
       len == 2;
       lock == 0;
       cache == 0;
       prot == 0;
       burst == 1;
       addr == 100;
-      foreach (data[i]) data[i] == 8'b11111111;
+      //foreach (data[i]) data[i] == 8'b11111111;
       foreach (strobe[i]) strobe[i] == 1;
     });
     $display("pkt_type=%s sop=0x%0h txn_id=0x%0h addr=0x%08h data = %p len=%0d size=%0d burst=%0d lock=%0d cache=%0d prot=%0d wr_en=%0b rd_en=%0b full=%0b empty=%0b wr_data=0x%032h rd_data=0x%032h eop=0x%0h data=%p strobe=%p",
